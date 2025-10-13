a015f7b47b809689100de91051eff506675a7ee4
fretka46 <fretka46.02@gmail.com>
2025-10-13 18:15:24 +0200

    Innit Commit

---
+++ src/main/java/com/fretka46/fShootingGame/FShootingGame.java
@@ -0,0 +1,17 @@
+package com.fretka46.fShootingGame;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FShootingGame extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
